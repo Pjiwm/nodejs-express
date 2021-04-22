e05630c7b2e665b4958ff80c3c2692e4b1beec2b
PMunne <pim.munne@gmail.com>
2021-04-22 15:00:47 +0200

    changed server.js with relevant data.

--- server.js
+++ server.js
@@ -2,21 +2,40 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-let result = {
-    "response": "hi 2 ",
-    "status": "ok 2",
-}
 
 
-app.get('/', (req, res) => {
-    res.status(200).json(result).end()
+
+app.get('/api/info', (req, res) => {
+    let student = {
+        "naam": "Pim Munne",
+        "studentnummer": "2170811",
+        "opleiding": "informatica",
+        "bescrhijving": "dit is een nodejs server voor samen eten",
+        "SonarQube": "",
+    }
+    res.status(200).json(student).end()
+})
+
+// GET method route
+app.get('/api', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/api', function (req, res) {
+    res.send('POST request to the homepage')
+})
+
+// PuT method route
+app.put('/api', function (req, res) {
+    res.send('POST request to the homepage')
 })
 
 
 
 
 app.listen(port, () => {
-    console.log(result)
+    console.log('server running')
 })
 
 // const http = require('http')
