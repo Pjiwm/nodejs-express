333e03684cc27796fcab01bdd2fbed47800524ef
PMunne <pim.munne@gmail.com>
2021-04-22 11:46:03 +0200

    added ignore file

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-const http = require('http')
-
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.status(200).json('Helllo world!').end()
+})
 
-const server = http.createServer((req, res) => {
+
     let result = {
         "response": "hi",
         "status": "ok",
     }
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(result))
+
+app.listen(port, () => {
+    console.log(result)
 })
 
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`)
-})
+// const http = require('http')
+
+// // const port = process.env.PORT || 3000
+
+
+
+
+
+// const server = http.createServer((req, res) => {
+//     let result = {
+//         "response": "hi",
+//         "status": "ok",
+//     }
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'application/json')
+//     res.end(JSON.stringify(result))
+// })
+
+// server.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`)
+// })
