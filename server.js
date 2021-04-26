902ecfdc99db452324183b9c2756d30a1a5bec6f
PMunne <Pim.Munne@gmail.com>
2021-04-26 10:50:12 +0200

    routing updated

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-
+app.all('*', (req, res, next) => {
+    const requestMethod = req.method
+    const reqUrl = req.url
+    console.log('called all:', requestMethod, reqUrl)
+    // res.status(200).json({message: "okay"})
+    next()
+})
 
 app.get('/api/info', (req, res) => {
     const student = {
@@ -14,6 +20,34 @@ app.get('/api/info', (req, res) => {
         SonarQube: null,
     }
     res.status(200).json(student).end()
+    console.log('called GET info')
+})
+
+app.get('/api/movies/:movieId', (req, res, next) => {
+    const movieId = req.params.movieId
+    if (movieId == 0) {
+        next({error: "movie does not exist", errorCode: 401})
+    }
+    console.log('called GET movie by ID')
+})
+
+
+app.all('*', (req, res) => {
+    const error = {
+        message: "Endpoint does not exist."
+    }
+    // res.status(400).json(error).end()
+    next({message: "Endpoint " + req.url+ " does not exist ", errorCode: 401})
+    console.log('called all: GET all')
+})
+
+app.use('*', (error, req, res, next) => {
+    console.log('Errorhandler called!')
+    console.log(error)
+    res.status(error.errorCode).json({
+        message: "Some error has occured",
+        error: error
+    })
 })
 
 // post method route
