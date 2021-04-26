0cd489bd81221c43c31e0df2f0363b9d8b6f9701
PMunne <Pim.Munne@gmail.com>
2021-04-26 14:03:40 +0200

    added seperate routing file

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const routes = require('./routes/routes.js')
 
 
-app.all('*', (req, res, next) => {
-    const requestMethod = req.method
-    const reqUrl = req.url
-    console.log('called all:', requestMethod, reqUrl)
-    // res.status(200).json({message: "okay"})
-    next()
-})
 
+// app.all('*', (req, res, next) => {
+//     const requestMethod = req.method
+//     const reqUrl = req.url
+//     console.log('called all:', requestMethod, reqUrl)
+//     next()
+// })
+// info 
 app.get('/api/info', (req, res) => {
     const student = {
         naam: "Pim Munne",
@@ -23,53 +24,38 @@ app.get('/api/info', (req, res) => {
     console.log('called GET info')
 })
 
-app.get('/api/movies/:movieId', (req, res, next) => {
-    const movieId = req.params.movieId
-    if (movieId == 0) {
-        next({error: "movie does not exist", errorCode: 401})
-    }
-    console.log('called GET movie by ID')
-})
-
-
-app.all('*', (req, res) => {
-    const error = {
-        message: "Endpoint does not exist."
-    }
-    // res.status(400).json(error).end()
-    next({message: "Endpoint " + req.url+ " does not exist ", errorCode: 401})
-    console.log('called all: GET all')
-})
-
-app.use('*', (error, req, res, next) => {
-    console.log('Errorhandler called!')
-    console.log(error)
-    res.status(error.errorCode).json({
-        message: "Some error has occured",
-        error: error
-    })
-})
-
-// post method route
-app.post('/api/post-test', (req, res) => {
-    res.send('GET request to the homepage')
-    const obj = req.body
-    console.log(obj)
-    res.status(200).json(obj).end()
-})
-
-// POST method route
-app.post('/api', function (req, res) {
-    res.send('POST request to the homepage')
-})
-
-// PuT method route
-app.put('/api', function (req, res) {
-    res.send('POST request to the homepage')
-})
+app.use('/', routes)
 
 
 
+
+
+// app.get('/api/movies/:movieId', (req, res, next) => {
+//     const movieId = req.params.movieId
+//     if (movieId == 0) {
+//         next({error: "movie does not exist", errorCode: 401})
+//     }
+//     console.log('called GET movie by ID')
+// })
+
+
+// app.all('*', (req, res) => {
+//     const error = {
+//         message: "Endpoint does not exist."
+//     }
+//     // res.status(400).json(error).end()
+//     next({message: "Endpoint " + req.url+ " does not exist ", errorCode: 401})
+//     console.log('called all: GET all')
+// })
+
+// app.use('*', (error, req, res, next) => {
+//     console.log('Errorhandler called!')
+//     console.log(error)
+//     res.status(error.errorCode).json({
+//         message: "Some error has occured",
+//         error: error
+//     })
+// })
 
 app.listen(port, () => {
     console.log('server running')
